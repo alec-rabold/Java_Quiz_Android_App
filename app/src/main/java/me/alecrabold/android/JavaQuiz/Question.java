7d12e8d5e037fab64b9e90e7ec482c8c588b2b2e
Alec Rabold <alecrabold@outlook.com>
2017-05-31 13:08:00 -0700

    Initial commit

---
+++ app/src/main/java/me/alecrabold/android/JavaQuiz/Question.java
@@ -0,0 +1,32 @@
+package me.alecrabold.android.JavaQuiz;
+
+/**
+ * Created by Alec on 5/8/2017.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
